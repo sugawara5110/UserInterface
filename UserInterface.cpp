7f96951abd3e01cab38653fe6b57d5bc3d94fa61
satoshi.s <satoshi.sugawara19790115@gmail.com>
2021-02-14 15:17:28 +0900

    修正

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -8,132 +8,243 @@
 #include "UserInterface.h"
 #include "../Common/DirectInputWrapper/DxInput.h"
 
-Meter::Meter() {
-	me = std::make_unique<PolygonData2D[]>(2);
+namespace {
+
+	class Meter {
+
+	public:
+		std::unique_ptr<PolygonData2D[]> me = nullptr;
+		int wid = 0;
+		int hei = 0;
+		int px = 0;
+		int py = 0;
+
+		bool ActionSt = false;
+		int CorrectionX = 0;
+		int CorrectionY = 0;
+		TCHAR HeaderString[STR_MAX_LENGTH];
+		int meterWid = 0;
+		float Ratio = 0.0f;
+		bool ActionStMeter = false;
+		int CorrectionMeterX = 0;
+		int Mov = 0;
+
+		Meter() {
+			me = std::make_unique<PolygonData2D[]>(2);
+		}
+		void create(int sizeW, int sizeH, char* headerString) {
+			wid = sizeW;
+			hei = sizeH;
+			int len = (int)strlen(headerString);
+			if (len > STR_MAX_LENGTH - 1)len = STR_MAX_LENGTH - 1;
+			mbstowcs(HeaderString, headerString, sizeof(TCHAR) * len);
+			HeaderString[len] = '\0';
+			me[0].GetVBarray2D(1);
+			me[1].GetVBarray2D(1);
+			me[0].CreateBox(0.0f, 0.0f, 0.0f, 0.1f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, true, true);
+			me[1].CreateBox(0.0f, 0.0f, 0.0f, 0.1f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, true, true);
+		}
+		bool checkRange() {
+			DxInput* di = DxInput::GetInstance();
+			int x = (int)di->PosX();
+			int y = (int)di->PosY();
+			if (px <= x && x <= px + wid &&
+				py <= y && y < py + hei * 0.5f)return true;
+
+			return false;
+		}
+		bool checkMeterRange() {
+			DxInput* di = DxInput::GetInstance();
+			int x = (int)di->PosX();
+			int y = (int)di->PosY();
+			if (px + 2.0f <= x && x <= px + wid - 2.0f &&
+				py + hei * 0.5f <= y && y <= py + hei - 2.0f)return true;
+
+			return false;
+		}
+		float updatePos(int posX, int posY, float depth, int meterMov) {
+			px = posX;
+			py = posY;
+			int mwidMax = wid - 4;
+			meterWid += meterMov;
+			if (meterWid < 0)meterWid = 0;
+			if (meterWid > mwidMax)meterWid = mwidMax;
+			Ratio = (float)meterWid / (float)mwidMax;
+			DxText::GetInstance()->
+				UpDateText(HeaderString, (float)px + 1.0f, (float)py + 1.0f, (float)hei * 0.3f, { 1.0f, 1.0f, 1.0f, 1.0f });
+			DxText::GetInstance()->
+				UpDateValue((int)(Ratio * 100.0f), (float)px + 1.0f, (float)py + 1.0f + hei * 0.5f, (float)hei * 0.3f,
+					3, { 1.0f, 1.0f, 1.0f, 1.0f });
+
+			me[0].Update((float)px, (float)py, depth + 0.02f,
+				0.0f, 0.3f, 1.0f, 1.0f, (float)wid, (float)hei);
+			me[1].Update((float)px + 2.0f, (float)py + hei * 0.5f, depth + 0.01f,
+				1.0f, 0.0f, 0.0f, 1.0f, (float)meterWid, hei * 0.5f - 2.0f);
+
+			return Ratio;
+		}
+		float updatePosMouse(float depth) {
+			DxInput* di = DxInput::GetInstance();
+			Mov = 0;
+			if (di->isLAction() && checkRange()) {
+				ActionSt = true;
+				CorrectionX = px - di->PosX();
+				CorrectionY = py - di->PosY();
+			}
+			if (ActionSt && di->isLDown()) {
+				px = di->PosX() + CorrectionX;
+				py = di->PosY() + CorrectionY;
+			}
+			else {
+				ActionSt = false;
+				CorrectionX = 0;
+				CorrectionY = 0;
+			}
+
+			if (di->isLAction() && checkMeterRange()) {
+				ActionStMeter = true;
+				CorrectionMeterX = di->PosX();
+			}
+			if (ActionStMeter && di->isLDown()) {
+				Mov = di->PosX() - CorrectionMeterX;
+				CorrectionMeterX = di->PosX();
+			}
+			else {
+				ActionStMeter = false;
+				CorrectionMeterX = 0;
+			}
+
+			return updatePos(px, py, depth, Mov);
+		}
+		void Draw(int com) {
+			me[0].SetCommandList(com);
+			me[1].SetCommandList(com);
+			me[0].Draw();
+			me[1].Draw();
+		}
+	};
+
+	Meter* me = nullptr;
+
+	class WindowObj {
+
+	public:
+		std::unique_ptr<PolygonData2D[]> win = nullptr;
+		int wid = 0;
+		int hei = 0;
+		int fontSize = 0;
+		int ChoicePos = 0;
+		int numChoice = 0;
+		int returnPos = 0;
+		TCHAR MenuString[STRTEX_MAX_PCS][STR_MAX_LENGTH] = {};
+
+		WindowObj() {
+			returnPos = -1;
+			win = std::make_unique<PolygonData2D[]>(1);
+		}
+		void create(int fontsize) {
+			fontSize = fontsize;
+			win[0].GetVBarray2D(1);
+			win[0].CreateBox(0.0f, 0.0f, 0.0f, 0.1f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, true, true);
+		}
+		void setMenuName(int numMenu, char** MenuArr) {
+			int maxCntX = 0;
+			int maxCntY = numMenu;
+			numChoice = numMenu;
+
+			for (int i = 0; i < numMenu; i++) {
+				int len = (int)strlen(MenuArr[i]);
+				if (len > STR_MAX_LENGTH - 1)len = STR_MAX_LENGTH - 1;
+				if (maxCntX < len)maxCntX = len;
+				mbstowcs(MenuString[i], MenuArr[i], sizeof(TCHAR) * len);
+				MenuString[i][len] = '\0';
+			}
+			wid = fontSize * maxCntX;
+			hei = fontSize * maxCntY;
+		}
+		int updatePos(int x, int y) {
+			returnPos = -1;
+			DxInput* di = DxInput::GetInstance();
+			switch (di->checkKeyActionNo()) {
+			case DIK_UP:
+				if (--ChoicePos < 0)ChoicePos = 0;
+				break;
+			case DIK_DOWN:
+				if (++ChoicePos > numChoice - 1)ChoicePos = numChoice - 1;
+				break;
+			case DIK_RCONTROL:
+				returnPos = ChoicePos;
+				break;
+			case DIK_DELETE:
+				returnPos = numChoice;
+				break;
+			}
+			win[0].Update((float)x, (float)y, 0.02f,
+				0.5f, 0.5f, 0.5f, 0.6f, (float)wid, (float)hei);
+			for (int i = 0; i < numChoice; i++)
+				if (i == ChoicePos)
+					DxText::GetInstance()->
+					UpDateText(MenuString[i], (float)x, (float)y + i * fontSize, (float)fontSize, { 1.0f, 1.0f, 1.0f, 1.0f });
+				else
+					DxText::GetInstance()->
+					UpDateText(MenuString[i], (float)x, (float)y + i * fontSize, (float)fontSize, { 0.0f, 0.0f, 1.0f, 1.0f });
+			return returnPos;
+		}
+		void Draw(int com) {
+			win[0].SetCommandList(com);
+			win[0].Draw();
+		}
+	};
+
+	WindowObj* win = nullptr;
 }
 
-void Meter::create(int sizeW, int sizeH, char* headerString) {
-	wid = sizeW;
-	hei = sizeH;
-	int len = (int)strlen(headerString);
-	if (len > 100)len = 100;
-	mbstowcs(HeaderString, headerString, sizeof(TCHAR) * len);
-	HeaderString[len] = '\0';
-	me[0].GetVBarray2D(1);
-	me[1].GetVBarray2D(1);
-	me[0].CreateBox(0.0f, 0.0f, 0.0f, 0.1f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, true, true);
-	me[1].CreateBox(0.0f, 0.0f, 0.0f, 0.1f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, true, true);
-}
-
-bool Meter::checkRange() {
-	DxInput* di = DxInput::GetInstance();
-	int x = (int)di->PosX();
-	int y = (int)di->PosY();
-	if (px <= x && x <= px + wid &&
-		py <= y && y < py + hei * 0.5f)return true;
-
-	return false;
-}
-
-bool Meter::checkMeterRange() {
-	DxInput* di = DxInput::GetInstance();
-	int x = (int)di->PosX();
-	int y = (int)di->PosY();
-	if (px + 2.0f <= x && x <= px + wid - 2.0f &&
-		py + hei * 0.5f <= y && y <= py + hei - 2.0f)return true;
-
-	return false;
-}
-
-float Meter::updatePos(int posX, int posY, float depth, int meterMov) {
-	px = posX;
-	py = posY;
-	int mwidMax = wid - 4;
-	meterWid += meterMov;
-	if (meterWid < 0)meterWid = 0;
-	if (meterWid > mwidMax)meterWid = mwidMax;
-	Ratio = (float)meterWid / (float)mwidMax;
-	DxText::GetInstance()->
-		UpDateText(HeaderString, (float)px + 1.0f, (float)py + 1.0f, (float)hei * 0.3f, { 1.0f, 1.0f, 1.0f, 1.0f });
-	DxText::GetInstance()->
-		UpDateValue((int)(Ratio * 100.0f), (float)px + 1.0f, (float)py + 1.0f + hei * 0.5f, (float)hei * 0.3f,
-			3, { 1.0f, 1.0f, 1.0f, 1.0f });
-
-	me[0].Update((float)px, (float)py, depth + 0.02f,
-		0.0f, 0.3f, 1.0f, 1.0f, (float)wid, (float)hei);
-	me[1].Update((float)px + 2.0f, (float)py + hei * 0.5f, depth + 0.01f,
-		1.0f, 0.0f, 0.0f, 1.0f, (float)meterWid, hei * 0.5f - 2.0f);
-
-	return Ratio;
-}
-
-float Meter::updatePosMouse(float depth) {
-	DxInput* di = DxInput::GetInstance();
-	Mov = 0;
-	if (di->isLAction() && checkRange()) {
-		ActionSt = true;
-		CorrectionX = px - di->PosX();
-		CorrectionY = py - di->PosY();
-	}
-	if (ActionSt && di->isLDown()) {
-		px = di->PosX() + CorrectionX;
-		py = di->PosY() + CorrectionY;
-	}
-	else {
-		ActionSt = false;
-		CorrectionX = 0;
-		CorrectionY = 0;
-	}
-
-	if (di->isLAction() && checkMeterRange()) {
-		ActionStMeter = true;
-		CorrectionMeterX = di->PosX();
-	}
-	if (ActionStMeter && di->isLDown()) {
-		Mov = di->PosX() - CorrectionMeterX;
-		CorrectionMeterX = di->PosX();
-	}
-	else {
-		ActionStMeter = false;
-		CorrectionMeterX = 0;
-	}
-
-	return updatePos(px, py, depth, Mov);
-}
-
-void Meter::Draw(int com) {
-	me[0].SetCommandList(com);
-	me[1].SetCommandList(com);
-	me[0].Draw();
-	me[1].Draw();
-}
-
-UserInterface::~UserInterface() {
+UserInterfaceMeter::~UserInterfaceMeter() {
 	ARR_DELETE(me);
 }
 
-void UserInterface::setWindowSize(int Width, int Height) {
-	windowWidth = Width;
-	windowHeight = Height;
-}
-
-void UserInterface::setNumMeter(int num) {
+void UserInterfaceMeter::setNumMeter(int num) {
 	numMeter = num;
 	me = new Meter[numMeter];
 }
 
-void UserInterface::createMeter(int index, int sizeW, int sizeH, char* headerString) {
+void UserInterfaceMeter::create(int index, int sizeW, int sizeH, char* headerString) {
 	me[index].create(sizeW, sizeH, headerString);
 }
 
-float UserInterface::updatePos(int index, int posX, int posY, float depth, int meterMov) {
+float UserInterfaceMeter::updatePos(int index, int posX, int posY, float depth, int meterMov) {
 	return me[index].updatePos(posX, posY, depth, meterMov);
 }
 
-float UserInterface::updatePosMouse(int index, float depth) {
+float UserInterfaceMeter::updatePosMouse(int index, float depth) {
 	return me[index].updatePosMouse(depth);
 }
 
-void UserInterface::Draw(int index, int com) {
+void UserInterfaceMeter::Draw(int index, int com) {
 	me[index].Draw(com);
+}
+
+UserInterfaceWindow::~UserInterfaceWindow() {
+	ARR_DELETE(win);
+}
+
+void UserInterfaceWindow::setNumWindow(int num) {
+	numWin = num;
+	win = new WindowObj[numWin];
+}
+
+void UserInterfaceWindow::create(int index, int fontSize) {
+	win[index].create(fontSize);
+}
+
+void UserInterfaceWindow::setMenuName(int index, int numMenu, char** MenuArr) {
+	win[index].setMenuName(numMenu, MenuArr);
+}
+
+int UserInterfaceWindow::updatePos(int index, int x, int y) {
+	return win[index].updatePos(x, y);
+}
+
+void UserInterfaceWindow::Draw(int index, int com) {
+	win[index].Draw(com);
 }
--- UserInterface.h
+++ UserInterface.h
@@ -9,52 +9,31 @@
 
 #include "../Common/Direct3DWrapper/DxText.h"
 
-class UserInterface;
-
-class Meter {
+class UserInterfaceMeter {
 
 private:
-	friend UserInterface;
-	std::unique_ptr<PolygonData2D[]> me = nullptr;
-	int wid = 0;
-	int hei = 0;
-	int px = 0;
-	int py = 0;
-
-	bool ActionSt = false;
-	int CorrectionX = 0;
-	int CorrectionY = 0;
-	TCHAR HeaderString[STR_MAX_LENGTH];
-	int meterWid = 0;
-	float Ratio = 0.0f;
-	bool ActionStMeter = false;
-	int CorrectionMeterX = 0;
-	int Mov = 0;
-
-	Meter();
-	void create(int sizeW, int sizeH, char* headerString);
-	bool checkRange();
-	bool checkMeterRange();
-	float updatePos(int posX, int posY, float depth, int meterMov);
-	float updatePosMouse(float depth);
-	void Draw(int com);
-};
-
-class UserInterface {
-
-private:
-	int windowWidth;
-	int windowHeight;
-	Meter* me = nullptr;
 	int numMeter = 0;
 
 public:
-	~UserInterface();
-	void setWindowSize(int Width, int Height);
+	~UserInterfaceMeter();
 	void setNumMeter(int num);
-	void createMeter(int index, int sizeW, int sizeH, char* headerString);
+	void create(int index, int sizeW, int sizeH, char* headerString);
 	float updatePos(int index, int posX, int posY, float depth, int meterMov);
 	float updatePosMouse(int index, float depth);
+	void Draw(int index, int com);
+};
+
+class UserInterfaceWindow {
+
+private:
+	int numWin = 0;
+
+public:
+	~UserInterfaceWindow();
+	void setNumWindow(int num);
+	void create(int index, int fontSize);
+	void setMenuName(int index, int numMenu, char** MenuArr);
+	int updatePos(int index, int x, int y);
 	void Draw(int index, int com);
 };
 
